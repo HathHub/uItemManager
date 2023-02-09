118f21ba01cb98b306e68ea178e656e8e9ac00db
Hath <66932866+HathHub@users.noreply.github.com>
2023-02-08 22:15:01 -0300

    a

---
+++ Class1.cs
@@ -0,0 +1,30 @@
+﻿
+using uScript.API.Attributes;
+using System.Net;
+using SDG.Unturned;
+using uScript.Module.Main.Classes;
+
+[ScriptModule("uItemManager")]
+public class uItemManager
+{
+    [ScriptFunction("askClearAllItems")]
+    public static void askClearAllItems()
+    {
+        ItemManager.askClearAllItems();
+        
+    }
+
+    [ScriptFunction("askClearRegionItems")]
+    public static void askClearRegionItems(byte x, byte y)
+    {
+        ItemManager.askClearRegionItems(x, y);
+
+    }
+
+    [ScriptFunction("ServerClearItemsInSphere")]
+    public static void ServerClearItemsInSphere(Vector3Class position, float radius)
+    {
+        ItemManager.ServerClearItemsInSphere(position.Vector3, radius);
+
+    }
+}
